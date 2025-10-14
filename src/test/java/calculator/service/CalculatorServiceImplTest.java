1ea3cef570b651c404f48d2a547af73314f41bb8
chyun7114 <chyun7114@naver.com>
2025-10-14 22:31:44 +0900

    test: splitBydelimeters 테스트 작성 - 기본 구분자 쉼표(,), 콜론(:)에 대한 문자열 분리 테스트 수행

---
+++ src/test/java/calculator/service/CalculatorServiceImplTest.java
@@ -0,0 +1,64 @@
+package calculator.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorServiceImplTest {
+
+    private CalculatorService calculatorService = new CaculatorServiceImpl();
+
+    @Nested
+    @DisplayName("구분자 분리 테스트")
+    class SplitByDelimetersTest {
+
+        @Test
+        @DisplayName("쉼표(,)로 구분된 문자열을 입력시 분리된 문자열 리스트가 반환된다.")
+        void success_split_string_by_rest() {
+
+            // given
+            String testInput = "1,2,3";
+            List<String> expected = List.of("1", "2", "3");
+
+            // when
+            List<String> result = calculatorService.splitByDelimeters(testInput);
+
+            // then
+            assertThat(result).containsAll(expected);
+        }
+
+        @Test
+        @DisplayName("콜론(:)으로 구분된 문자열을 입력시 분리된 문자열 리스트가 입력된다.")
+        void success_split_string_by_colon() {
+
+            // given
+            String testInput = "1:2:3";
+            List<String> expected = List.of("1", "2", "3");
+
+            // when
+            List<String> result = calculatorService.splitByDelimeters(testInput);
+
+            // then
+            assertThat(result).containsAll(expected);
+        }
+
+        @Test
+        @DisplayName("쉼표와 콜론이 함께 입력된 문자열을 입력시 분리된 문자열 리스트가 입력된다.")
+        void success_split_string_by_two_delimeters() {
+
+            // given
+            String testInput = "1:2,3";
+            List<String> expected = List.of("1", "2", "3");
+
+            // when
+            List<String> result = calculatorService.splitByDelimeters(testInput);
+
+            // then
+            assertThat(result).containsAll(expected);
+        }
+    }
+}
