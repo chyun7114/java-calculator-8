31252796212ddf41274265b0d24164002870bfa7
chyun7114 <chyun7114@naver.com>
2025-10-15 17:41:11 +0900

    feat: Calculator 도메인 작성 - 분리된 숫자 값의 합을 반환하는 메소드 작성

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -4,5 +4,28 @@ import java.util.List;
 
 public class Calculator {
 
-    List<String> numbers;
+    private double result;
+
+    public Calculator() {
+
+        this.result = 0;
+    }
+
+    public double getResult() {
+
+        return this.result;
+    }
+
+    public void calculate(List<String> numbers) {
+
+        if(numbers.isEmpty()) return;
+
+        for(String number : numbers) {
+            try {
+                this.result = Double.parseDouble(number);
+            } catch(NumberFormatException e) {
+                throw new IllegalArgumentException("잘못된 값 입력");
+            }
+        }
+    }
 }
