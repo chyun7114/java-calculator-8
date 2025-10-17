6ec1ca4a8a017ab5cb5d20c673ab0d8311cb6341
chyun7114 <chyun7114@naver.com>
2025-10-17 22:23:48 +0900

    style: 우테코 코드 스타일 적용

--- src/main/java/calculator/view/ConsoleView.java
+++ src/main/java/calculator/view/ConsoleView.java
@@ -2,7 +2,7 @@ package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class ConsoleView{
+public class ConsoleView {
 
     public String getUserInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
